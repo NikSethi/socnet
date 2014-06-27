5e39c0caf70e817a9f3def7a0976a716c297b97f
NikSethi <nikhilsethi108@gmail.com>
2014-06-27 15:11:46 -0400

    added login, registration pages to navbar and improved how they look

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'simple_form'
 gem 'devise', github: 'plataformatec/devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
+
   resources :statuses
   root 'statuses#index'
   # The priority is based upon order of creation: first created -> highest priority.
